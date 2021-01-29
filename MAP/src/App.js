731e742aa2fa61f1c527fd5981e49bfa2c2dfe18
1337.ishaan <1337.ishaan@gmail.com>
2021-01-29 18:04:27 +0000

    added datepicker and timeslider

--- MAP/src/App.js
+++ MAP/src/App.js
@@ -2,14 +2,28 @@ import './App.scss';
 import { useState } from 'react';
 import { Map } from './components/Map/Map';
 import { LoadingAnimation } from './components/LoadingAnimation/LoadingAnimation';
+import TimeSlider from "./components/TimeSlider/TimeSlider"
+
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
+  const [haveSelectedDate, setHaveSelectedDate] = useState(false);
+  const [startDate, setStartDate] = useState(false);
+  const [endDate, setEndDate] = useState(false);
+  const [currentLanguage, setCurrentLanguage] = useState({ t: (text) => text });
+  
 
+
+  const __onSelectDate = (selectedDate, startDate, endDate) => {
+    setHaveSelectedDate(selectedDate);
+    setStartDate(startDate);
+    setEndDate(endDate);
+  };
   return (
     <>
       <LoadingAnimation isLoading={isLoading} />
       <Map setIsLoading={setIsLoading}></Map>
+      <TimeSlider onChange={__onSelectDate} i18n={currentLanguage}/>
     </>
   );
 }
